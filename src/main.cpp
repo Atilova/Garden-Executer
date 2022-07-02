a531877ef34d820cd0acdcfc965fc51775bec184
Atilova <ati2lova3@gmail.com>
2022-07-02 22:00:43 +0300

    Added json workflow for adding values to any doc

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,52 @@
 #include <Arduino.h>
 #include <Adafruit_MCP23017.h>
 #include <modules/sensors.h>
+#include <ArduinoJson.h>
+#include <Esp.h>
 
 #define len(object) sizeof(object) / sizeof(*object)
 
 
+StaticJsonDocument<500> doc;
+
 Adafruit_MCP23017 sensorsBoard;
-
-BaseSensor* sensorsList[] = {
-  new SensorI2C {"SENSOR1", sensorsBoard, 5},
-  new OneWireSensor {"SENSOR2", 1},
-  new OneWireSensor {"SENSOR4", 3}
+AbstractSensor* sensorsList[] = {
+  new SensorI2C {"SENSOR1", "tank.level.test", sensorsBoard, 5},  
+  new SensorI2C {"SENSOR2", "feedback.ac", sensorsBoard, 2},  
+  new OneWireSensor {"SENSOR3", "weather.humidity", 1},  
 };
 
-Sensors sensors(sensorsList, len(sensorsList));
+Sensors sensors(sensorsList, len(sensorsList), "sensors", doc);
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  delay(1000);
 
-  sensors.get("SENSOR1");
-  sensors.get("SENSOR2");
-};
 
-void loop() {
+void measure() {
+  Serial.println("Measuring...");
+  Serial.println(ESP.getFreeHeap());
+  sensors.measure("SENSOR1");
+  Serial.println(ESP.getFreeHeap());
+  sensors.measure("SENSOR2");
+  Serial.println(ESP.getFreeHeap());
+  sensors.measure("SENSOR3");
+  Serial.println(ESP.getFreeHeap());
+  sensors.measure("SENSOR4");
+  Serial.println(ESP.getFreeHeap());
+  serializeJsonPretty(doc, Serial);
+}
 
-};
+void setup() 
+  {
+    Serial.begin(115200);
+    Serial.println();
+    delay(1000);
+
+    // measure();
+  };
+
+
+
+void loop() 
+  {
+    measure();
+    delay(300);
+  };
---
+++ src/modules/JsonFlow.h
@@ -0,0 +1,37 @@
+#ifndef _JSON_FLOW_H_
+  #define _JSON_FLOW_H_
+
+  #include <ArduinoJson.h>
+
+
+  class JsonWorkflow 
+    {
+      public:
+        static int setValue(JsonDocument& doc, const char* section, char** levels, uint8_t nestedLevelSize, auto value)
+          {
+            auto docSection = doc[section];
+            switch(nestedLevelSize)
+              {
+                case(1):
+                  docSection[levels[0]] = value;
+                  break;
+
+                case(2):
+                  docSection[levels[0]][levels[1]] = value;
+                  break;
+
+                case(3):
+                docSection[levels[0]][levels[1]][levels[2]] = value;
+                  break;
+
+                case(4):
+                  docSection[levels[0]][levels[1]][levels[2]][levels[3]] = value;
+                  break;
+
+                default:
+                  return -1;
+              };
+            return true;
+          };          
+    };            
+#endif
