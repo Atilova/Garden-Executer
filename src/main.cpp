7a75e3e0aa4adc52db9b1c9c23d9bbfdc3c6110f
Atilova <ati2lova3@gmail.com>
2022-07-03 16:24:25 +0300

    main.cpp updated, added relays setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <Adafruit_MCP23017.h>
-#include <modules/sensors.h>
+#include <modules/Init.h>
 #include <ArduinoJson.h>
 #include <Esp.h>
 
@@ -8,45 +8,46 @@
 
 
 StaticJsonDocument<500> doc;
+Adafruit_MCP23017 powerBoardI2C,
+                  sensorsBoardI2C;
 
-Adafruit_MCP23017 sensorsBoard;
-AbstractSensor* sensorsList[] = {
-  new SensorI2C {"SENSOR1", "tank.level.test", sensorsBoard, 5},  
-  new SensorI2C {"SENSOR2", "feedback.ac", sensorsBoard, 2},  
-  new OneWireSensor {"SENSOR3", "weather.humidity", 1},  
-};
+RelayController relays;
+SensorsController sensors;
 
-Sensors sensors(sensorsList, len(sensorsList), "sensors", doc);
+Relay relaysList[] =
+  {
+    Relay {"SYSTEM_5V_RELAY", "5v", powerBoardI2C, 0},
+    Relay {"SYSTEM_12V_RELAY", "12v", powerBoardI2C, 1},
+    Relay {"SYSTEM_24V_RELAY", "24v", powerBoardI2C, 2},
+    Relay {"SYSTEM_CONTACTOR", "contactor", powerBoardI2C, 3},
+    Relay {"SYSTEM_WELL_PUMP", "wellPump", powerBoardI2C, 4}
+  };
+
+AbstractSensor* sensorsList[] =
+  {
+    new SensorI2C {"SENSOR1", "tank.level.test", sensorsBoardI2C, 5},
+    new SensorI2C {"SENSOR2", "feedback.ac", sensorsBoardI2C, 2},
+    new OneWireSensor {"SENSOR3", "weather.humidity", 1}
+  };
 
 
-
-void measure() {
-  Serial.println("Measuring...");
-  Serial.println(ESP.getFreeHeap());
-  sensors.measure("SENSOR1");
-  Serial.println(ESP.getFreeHeap());
-  sensors.measure("SENSOR2");
-  Serial.println(ESP.getFreeHeap());
-  sensors.measure("SENSOR3");
-  Serial.println(ESP.getFreeHeap());
-  sensors.measure("SENSOR4");
-  Serial.println(ESP.getFreeHeap());
-  serializeJsonPretty(doc, Serial);
-}
-
-void setup() 
+void setup()
   {
     Serial.begin(115200);
     Serial.println();
-    delay(1000);
 
-    // measure();
+    Wire.begin();
+    powerBoardI2C.writeGPIOAB(0xffff);
+    powerBoardI2C.begin(0x0);  // плата управления силовыми реле сист. полива
+    sensorsBoardI2C.begin(0x2);  // сигналы от датчиков обратной связи
+
+    relays.configure(relaysList, len(relaysList), "relays", doc);
+    sensors.configure(sensorsList, len(sensorsList), "sensors", doc);
   };
 
 
 
-void loop() 
+void loop()
   {
-    measure();
-    delay(300);
+
   };
