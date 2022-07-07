acd507ec9abf26c80f17a2abd985057f4de3f888
Miha <ati2lova3@gmail.com>
2022-07-07 20:35:29 +0300

    Added measuring function for (garden pump water pressure) sensor

--- src/modules/sensors.h
+++ src/modules/sensors.h
@@ -155,7 +155,7 @@
       struct SensorWaterFlow : private BaseSensor<float>,
                                public AbstractSensor
         {
-          // static const int ERROR = -127;
+          static const int ERROR = 0;
           private:
             uint8_t pinInput;
 
@@ -176,6 +176,33 @@
                     return;
                   };
                 uint16_t currentValue = round(1000000 / totalPulse);
+                checkDiffer(currentValue, diff) && setValue(doc, section, currentValue);
+              };
+        };
+
+      struct SensorWaterPressure : private BaseSensor<float>,
+                                   public AbstractSensor
+        {          
+          private:            
+            static const uint8_t zeroPressureK = 0.17;
+            static const uint16_t resistanceR1 = 1000,  // делитель 5v -> 3.3v
+                                  resistanceR2 = 2000;
+            uint8_t pinInput;
+
+          public:
+            SensorWaterPressure(const char* name, const String& levels, uint8_t pinInput) :
+              AbstractSensor(name, levels)
+              {
+                this->pinInput = pinInput;                
+              };
+
+            virtual void measure(JsonDocument& doc, const char* section, boolean diff) override
+              {                
+                float decimalValue = analogRead(pinInput) * 3.333 / 4096,
+                      calculatedPressure = (decimalValue - zeroPressureK) * 3.947693,  // Переводим в АТМФ
+                      currentValue = calculatedPressure * (resistanceR1+resistanceR2) / resistanceR2;  // Масштабируем за счет делителя
+
+                currentValue = currentValue > 0 ? 0.00 : currentValue;
                 checkDiffer(currentValue, diff) && setValue(doc, section, currentValue);
               };
         };
