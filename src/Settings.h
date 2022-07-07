6b005389358326a501612f88c4f4588622a19d62
Atilova <ati2lova3@gmail.com>
2022-07-08 02:52:03 +0300

    Added config for mqtt & Esp32Ping lib

--- src/Settings.h
+++ src/Settings.h
@@ -1,4 +1,34 @@
-#ifndef
+#ifndef _SETTINGS_H_
   #define _SETTINGS_H_
+  
+  #include <Arduino.h>
+  #include "Secrete.h"
+  #include <modules/Mqtt.h>
+
+  #define len(object) sizeof(object) / sizeof(*object)
+
+
+  const char* topicsSubscribe[] = 
+    {
+      "garden/tester",
+      "garden/alarm"
+    };
+
+  mosquitto::Config mqttConfig = 
+    {
+      .listeners=topicsSubscribe,
+      .listenersSize=len(topicsSubscribe),
+      .host="192.168.1.13",
+      .port=1883,
+      .keepAlive=10,
+      .socketTimeout=12,
+      .clientId="EXECUTER_HOST",
+      .mqttUsername=MQTT_USERNAME,
+      .mqttPassword=MQTT_PASSWORD,
+      .wifiPoint="Tenda_62176",
+      // .wifiPoint="we..",
+      .wifiPassword=WIFI_PASSWORD
+    };
+
 
 #endif
