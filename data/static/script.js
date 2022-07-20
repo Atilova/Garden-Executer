135d60b9e1f2ce43a1da2eb798245ed66e8b9bb4
Atilova <ati2lova3@gmail.com>
2022-07-20 03:22:54 +0300

    Pre-Release config web version, added weather block

--- data/static/script.js
+++ data/static/script.js
@@ -1,65 +1,129 @@
 const l = (...args) => console.log("Info -> ", ...args);
 
 
-const configWorkflow = () => {
-  const $configIndoorLi = document.querySelector("#config-indoor"),
-        $configNoiseLi = document.querySelector("#config-noise"),
-        $configDisturberLi = document.querySelector("#config-disturber"),
-        $configThresholdLi = document.querySelector("#config-threshold"),
-        $configSpikeLi = document.querySelector("#config-spike"),
-        $configLightningsLi = document.querySelector("#config-lightnings");
+const managementWorkflow = () => {
+  class FetchingState {
+    static LOADING = 0;
+    static FETCHED = 1;
+    static FAILED = -1;
+  };
+  
+  const configArray = [
+    [document.querySelector("#config-indoor"), "indoor", ["outdoor", "indoor"]],
+    [document.querySelector("#config-noise"), "noiseLevel"],
+    [document.querySelector("#config-disturber"), "showDisturber", ["off", "on"]],
+    [document.querySelector("#config-threshold"), "watchdogThreshold"],
+    [document.querySelector("#config-spike"), "spike"],
+    [document.querySelector("#config-lightnings"), "lightningsCount"],
+  ];
 
-  const $applyConfigButton = document.querySelector("#config-apply");
-    
+  const $applyConfigButton = document.querySelector("#config-apply"),
+        $refreshConfigButton = document.querySelectorAll("#config-refresh"),
+        $editConfigButton = document.querySelector("#config-edit"),
+        $exitConfigButton = document.querySelector("#config-exit");
+
   const fetchConfig = () => {
-    fetch("http://192.168.1.112/api/options",)
+    fetch("http://192.168.1.112/api/options")
       .then(response => response.json())
       .then(data => {
-        setSelectOption($configIndoorLi, data.indoor ? "indoor" : "outdoor");
-        setSelectOption($configNoiseLi, data.noiseLevel);
-        setSelectOption($configDisturberLi, data.showDisturber ? "on" : "off");
-        setSelectOption($configThresholdLi, data.watchdogThreshold);
-        setSelectOption($configSpikeLi, data.spike);
-        setSelectOption($configLightningsLi, data.lightningsCount);
-      });
+        configArray.forEach(setting => {
+          const [$htmlElement, name, params] = setting;
+          setSelectOption($htmlElement, !!params ? params[data[name]] : data[name]);
+        });
+      })
+      .catch(error => setTimeout(
+        () => configArray.forEach(setting => setLoader(FetchingState.FAILED, setting[0])),
+        100
+      ));
   };
 
-  const setLoader = (state, $parentElement) => {    
-    $parentElement.querySelector(".spark-options__loader").style.display = state ? "block" : "none";
-    $parentElement.querySelector("select").style.display = state ? "none" : "block";
-
+  const setLoader = (state, $parentElement) => {
     const loadingClass = "spark-options__setting_loading";
-    setTimeout(() => state ? $parentElement.classList.add(loadingClass): $parentElement.classList.remove(loadingClass), 0);
+
+    const setDisplayAll = ($select, $loader, $error) => {      
+      $parentElement.querySelector("select").style.display = $select ? "block" : "none";
+      $parentElement.querySelector(".spark-options__loader").style.display = $loader ? "block" : "none";
+      $parentElement.querySelector("i").style.display = $error ? "block" : "none";
+    };
+
+    if(state == FetchingState.LOADING) {
+      setDisplayAll(false, true, false);
+      setTimeout(() => {
+        $parentElement.classList.remove("spark-options__setting_failed");
+        $parentElement.classList.add(loadingClass);
+      }, 50);
+    }
+    else if(state == FetchingState.FETCHED) {
+      setDisplayAll(true, false, false);      
+      setTimeout(() => {
+        $parentElement.classList.remove("spark-options__setting_failed");
+        $parentElement.classList.remove(loadingClass);
+      });
+    }
+    else if(state == FetchingState.FAILED) {
+      setDisplayAll(false, false, true);
+      setTimeout(() => {
+        $parentElement.classList.remove(loadingClass);
+        $parentElement.classList.add("spark-options__setting_failed");
+      }, 50);
+    };
   };
 
-  const getSelectElementValue = $parentElement => {    
-    return $parentElement.querySelector("select").value;
+  const getSelectElement = $parentElement => {
+    return $parentElement.querySelector("select");
   };
 
-  const setSelectOption = ($parentElement, value) => {    
+  const setSelectOption = ($parentElement, value) => {
     $parentElement.querySelector("select").value = value;
-    setLoader(false, $parentElement);    
+    setLoader(FetchingState.FETCHED, $parentElement);
+  };
+
+  const allowEdit = allow => {
+    configArray.forEach(setting => {
+      const $element = getSelectElement(setting[0]);
+
+      $element.disabled = !allow;
+      if(allow)
+        $element.style.textAlign = "center";
+      else
+        $element.style.textAlign = "end";
+      });
+
+    document.querySelectorAll(".spark-options__menu").forEach(($menu) => {
+      allow ? $menu.classList.add("spark-options__menu_next") : $menu.classList.remove("spark-options__menu_next");
+    });
+  };
+
+  const refreshConfig = () => {
+    configArray.forEach(setting => setLoader(FetchingState.LOADING, setting[0]));
+    setTimeout(fetchConfig, 150);
   };
 
   $applyConfigButton.addEventListener("click", () => {
-    jsonToServer = JSON.stringify({
-      "indoor": getSelectElementValue($configIndoorLi) == "indoor" ? true : false,
-      "noiseLevel": +getSelectElementValue($configNoiseLi),
-      "showDisturber": getSelectElementValue($configDisturberLi) == "on" ? true : false,
-      "watchdogThreshold": +getSelectElementValue($configThresholdLi),
-      "spike": +getSelectElementValue($configSpikeLi),
-      "lightningsCount": +getSelectElementValue($configLightningsLi)
+    jsonToServer = {};
+    configArray.forEach(setting => {
+      const [$htmlElement, name, params] = setting,
+            currentValue = getSelectElement($htmlElement).value;
+      jsonToServer[name] = !!params ? !!params.indexOf(currentValue) : +currentValue;
+      setLoader(FetchingState.LOADING, $htmlElement);
     });
-
-    fetch("http://192.168.1.112/api/options", {"method": "POST", "body": jsonToServer});
+    fetch("http://192.168.1.112/api/options", {"method": "POST", "body": JSON.stringify(jsonToServer)});
+    setTimeout(fetchConfig, 150);
   });
 
+  $refreshConfigButton.forEach($button => $button.addEventListener("click", refreshConfig));
+
+  $editConfigButton.addEventListener("click", () => allowEdit(true));
+
+  $exitConfigButton.addEventListener("click", () => {
+    allowEdit(false);
+    refreshConfig();
+  });
+  
   fetchConfig();
 };
 
-
-
-configWorkflow();
+managementWorkflow();
 
 
 
@@ -133,3 +197,11 @@ configWorkflow();
 //   0
 // );
 
+
+
+
+// $parentElement.querySelector(".spark-options__loader").style.display = state == FetchingState.LOADING ? "block" : "none";
+// $parentElement.querySelector("select").style.display = state == FetchingState.LOADING ? "none" : "block";
+
+// const loadingClass = "spark-options__setting_loading";
+// setTimeout(() => state == FetchingState.LOADING ? $parentElement.classList.add(loadingClass): $parentElement.classList.remove(loadingClass), 0);
