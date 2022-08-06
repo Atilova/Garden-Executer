cb1179b32a06f9d40e103356f2661e8f85920900
Atilova <ati2lova3@gmail.com>
2022-08-07 01:36:58 +0300

    Added handshake, pong events; inbox, send buffers

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,30 @@
 #include <Arduino.h>
-#include <Adafruit_MCP23017.h>
-#include <modules/Init.h>
 #include <ArduinoJson.h>
 #include <Settings.h>
-#include <Secrete.h>
 #include <modules/Mqtt.h>
-#include <Esp.h>
 
 
+Mqtt::MqttConsumer mqttConsumer;
+Mqtt::TopicPublisher dashboardPublisher(mqttConfig.SERVER_TOPIC_PUBLISH, mqttConsumer);
 
 
-mosquitto::MqttConsumer mqttEngine;
-
-StaticJsonDocument<500> doc;
-Adafruit_MCP23017 powerBoardI2C,
-                  sensorsBoardI2C;
-
-RelayController relays;
-SensorsController sensors;
-
-Relay relaysList[] =
+void callback(char* topic, JsonDocument& payload)
   {
-    Relay {"SYSTEM_5V_RELAY", "5v", powerBoardI2C, 0},
-    Relay {"SYSTEM_12V_RELAY", "12v", powerBoardI2C, 1},
-    Relay {"SYSTEM_24V_RELAY", "24v", powerBoardI2C, 2},
-    Relay {"SYSTEM_CONTACTOR", "contactor", powerBoardI2C, 3},
-    Relay {"SYSTEM_WELL_PUMP", "wellPump", powerBoardI2C, 4}
+    Serial.print("Topic -> ");
+    Serial.println(topic);
+    dashboardPublisher.yield("RETAIN");
   };
-
-AbstractSensor* sensorsList[] =
-  {
-    new SensorI2C {"SENSOR1", "tank.level.test", sensorsBoardI2C, 5},
-    new SensorI2C {"SENSOR2", "feedback.ac", sensorsBoardI2C, 2},
-    new OneWireSensor {"SENSOR3", "weather.humidity", 1}
-  };
-
 
 void setup()
   {
     Serial.begin(115200);
     Serial.println();
 
-    Wire.begin();    
-    powerBoardI2C.writeGPIOAB(0xfff);
-    powerBoardI2C.begin(0x0);  // плата управления силовыми реле сист. полива
-    sensorsBoardI2C.begin(0x2);  // сигналы от датчиков обратной связи
-    
-    relays.configure(relaysList, len(relaysList), "relays", doc);
-    sensors.configure(sensorsList, len(sensorsList), "sensors", doc);
-
-    mqttEngine.configure(mqttConfig);
-
-
-  //   delay(1000);
-  //   relays.setState("SYSTEM_5V_RELAY", true);
-  //   relays.setState("SYSTEM_CONTACTOR", true);
-  //   relays.setState("SYSTEM_12V_RELAY", true);
-
-  //   sensors.measureAll();
-  //   relays.readAll();
-  //   serializeJsonPretty(doc, Serial);
-
-    mqttEngine.start();
+    mqttConsumer.configure(mqttConfig, callback);
+    mqttConsumer.run();
   };
 
 void loop()
   {
-   
+
   };
--- src/modules/Mqtt.h
+++ src/modules/Mqtt.h
@@ -5,51 +5,84 @@
   #include <PubSubClient.h>
   #include <Wifi.h>
   #include <ESP32Ping.h>
+  #include <IPAddress.h>
+  #include <Functions.h>
+  #include <CircularBuffer.h>
+  #include <functional>
+  #include <ArduinoJson.h>
+  #include <ESPRandom.h>
 
 
-  namespace mosquitto
+  namespace Mqtt
     {
-      #define MQTT_RECEIVE_CALLBACK std::function<void(char*, uint8_t*, unsigned int)> receiveCallback
-      #define repeat(n) for(int i = n; i--;)
-      // #define portMAX_DELAY TickType_t 0xffffffffUL
+      typedef std::function<void(char* topic, JsonDocument& doc)> receiveCallback_t;
 
       struct Config {
-        const char** listeners;
-        const uint32_t listenersSize;
-        const char* host;
-        const uint16_t port;
-        const uint8_t keepAlive;
-        const uint8_t socketTimeout;
-        const char* clientId;
-        const char* mqttUsername;
-        const char* mqttPassword;
-        const char* wifiPoint;
-        const char* wifiPassword;
+        IPAddress WIFI_IP;
+        IPAddress WIFI_GATEWAY;
+        IPAddress WIFI_SUBNET_MASK;
+        IPAddress WIFI_PRIMARY_DNS;
+        IPAddress WIFI_SECONDARY_DNS;
+        const char* WIFI_SSID;
+        const char* WIFI_PASSWORD;
+        const char* SERVER_TOPIC_LISTEN;
+        const char* SERVER_TOPIC_PUBLISH;
+        const char** LISTENERS;
+        const uint32_t LISTENERS_SIZE;
+        const char* HOST;
+        const uint16_t PORT;
+        const uint8_t KEEPALIVE;
+        const uint8_t SOCKET_TIMEOUT;
+        const char* CLIENT_ID;
+        const char* MQTT_USERNAME;
+        const char* MQTT_PASSWORD;
       };
 
+       struct PublishBufferData
+        {
+          const char* topic;
+          const char* payload;
+          boolean retain;
+        };
+
+      struct InboxBufferData
+        {
+          char* topic;
+          byte* payload;
+          uint8_t length;
+        };
 
       class MqttConsumer : private PubSubClient
         {
-          static const uint8_t wifiConnectionTimeout = 7,  // In Seconds
-                               wifiRetryEntries = 3,  // Entries count, before WiFi will be reset
-                               nextPingDelay = 2;  // In Seconds
+          protected:
+            constexpr static const char* HANDSHAKE_MESSAGE_TEMPLATE = "{\"type\": \"HANDSHAKE\", \"id\": \"%s\"}";
+            constexpr static const char* PONG_MESSAGE_TEMPLATE = "{\"type\": \"PONG\"%s}";
+
+            constexpr static const uint8_t WIFI_CONNECTION_TIMEOUT = 3,  // In Seconds
+                                           WIFI_RETRY_ENTRIES = 5,  // Entries count, before WiFi will be reset
+                                           NEXT_PING_DELAY = 2,  // In Seconds
+                                           HANDSHAKE_TIMEOUT = 4;  // In Seconds
 
           private:
-            Config* _configuration;
+            Config* _conf;
             WiFiClient wifiClient;
+            CircularBuffer<PublishBufferData, 100> publishBuffer;
+            CircularBuffer<InboxBufferData, 20> inboxBuffer;
+            StaticJsonDocument<1000> jsonBuffer;
             TaskHandle_t xEngineHandle = NULL;
+            TaskHandle_t xPublisherHandle = NULL;
+            TaskHandle_t xInboxHandle = NULL;
+            receiveCallback_t userReceiveCallback;
+            String lastId;
             void (MqttConsumer::* handleNext)(void) = &MqttConsumer::connectToWifi;
+            volatile boolean isHandshaked = false;
 
-            static void mainLoop(void* anyParameter)
+            static void engineLoop(void* consumer)
               {
-                MqttConsumer* _instance = (MqttConsumer*) anyParameter;
+                MqttConsumer* _instance = static_cast<MqttConsumer*>(consumer);
+
                 for(;;)
                   (_instance->*(_instance->handleNext))();
-              };
-
-            uint32_t calculateTicsTime(uint32_t realTime)
-              {
-                return realTime / portTICK_PERIOD_MS;
               };
 
             void clearWifiConnection(void)
@@ -61,75 +94,240 @@
 
             void connectToWifi(void)
               {
-                const uint16_t timeoutDelay = calculateTicsTime(wifiConnectionTimeout*1000L);
+                const uint16_t timeoutDelay = calculateTicsTime(WIFI_CONNECTION_TIMEOUT*1000L);
+                WiFi.config(_conf->WIFI_IP, _conf->WIFI_GATEWAY, _conf->WIFI_SUBNET_MASK, _conf->WIFI_PRIMARY_DNS, _conf->WIFI_SECONDARY_DNS);
                 while(!WiFi.isConnected())
                   {
                     clearWifiConnection();
-                    repeat(wifiRetryEntries)
+                    repeat(WIFI_RETRY_ENTRIES)
                       {
                         if(WiFi.isConnected())
                           break;
-                        WiFi.begin(_configuration->wifiPoint, _configuration->wifiPassword);
+                        WiFi.begin(_conf->WIFI_SSID, _conf->WIFI_PASSWORD);
                         vTaskDelay(timeoutDelay);
                       };
                   };
                 Serial.println("WIFI CONNECTED");
-                handleNext = &MqttConsumer::pingServerOverWifi;
+                handleNext = &MqttConsumer::pingServer;
               };
 
-            void pingServerOverWifi(void)
+            void pingServer(void)
               {
-                const uint16_t pingDelay = calculateTicsTime(nextPingDelay*1000L);
+                const uint16_t pingDelay = calculateTicsTime(NEXT_PING_DELAY*1000L);
                 while(WiFi.isConnected())
                   {
-                    if(Ping.ping(_configuration->host, 3))
-                      break;
+                    if(Ping.ping(_conf->HOST, 3))
+                      {
+                        handleNext = &MqttConsumer::connectToServer;
+                        return;
+                      };
                     vTaskDelay(pingDelay);
                   };
 
-                if(!WiFi.isConnected())
-                  handleNext = &MqttConsumer::connectToWifi;
-                else
-                  handleNext = &MqttConsumer::connectToServer;
+                handleNext = &MqttConsumer::connectToWifi;
               };
 
             void connectToServer(void)
               {
-                if(connect(_configuration->clientId, _configuration->mqttUsername, _configuration->mqttPassword))
+                if(connect(_conf->CLIENT_ID, _conf->MQTT_USERNAME, _conf->MQTT_PASSWORD))
                   {
                     Serial.println("Mqtt Connected");
-                    for(uint8_t index = 0; index < _configuration -> listenersSize; index++)
-                      subscribe(_configuration -> listeners[index]);
+                    subscribe(_conf->SERVER_TOPIC_LISTEN);
+                    for(uint8_t index = 0; index < _conf->LISTENERS_SIZE; index++)
+                      subscribe(_conf->LISTENERS[index]);
 
-                    const uint16_t loopDelay = calculateTicsTime(200);
+                    callPublisher();
+                    const uint16_t loopDelay = calculateTicsTime(100);
+
+                    uint32_t lastTimeHandshaked = 0;
                     while(loop())
-                      vTaskDelay(loopDelay);
+                      {
+                        if(!isHandshaked && (millis() - lastTimeHandshaked) > HANDSHAKE_TIMEOUT*1000L)
+                          {
+                            lastTimeHandshaked = millis();
+
+                            uint8_t uuid[16];
+                            ESPRandom::uuid4(uuid);
+                            lastId = ESPRandom::uuidToString(uuid);
+
+                            char buffer[200];
+                            sprintf(buffer, HANDSHAKE_MESSAGE_TEMPLATE, lastId.c_str());
+                            publish(_conf->SERVER_TOPIC_PUBLISH, buffer);  // Use simple publish instead smartPublish to prevent garbage data after connection fail
+                          };
+                        vTaskDelay(loopDelay);
+                      };
                   }
                   else
                     vTaskDelay(calculateTicsTime(1000));
 
-                  handleNext = &MqttConsumer::pingServerOverWifi;
+                  isHandshaked = false;
+                  handleNext = &MqttConsumer::pingServer;
+              };
+
+            void callPublisher(void)
+              {
+                if(xPublisherHandle != NULL)
+                  return;
+
+                xTaskCreate(
+                  [](void* consumer)
+                  {
+                    MqttConsumer* _instance = static_cast<MqttConsumer*>(consumer);
+                    const uint16_t delayBeforeNext = calculateTicsTime(100);
+                    while(_instance->connected() && !_instance->publishBuffer.isEmpty())
+                      {
+                        PublishBufferData data = _instance->publishBuffer.pop();
+                        _instance->publish(data.topic, data.payload, data.retain);
+                        vTaskDelay(delayBeforeNext);
+                      };
+                    _instance->xPublisherHandle = NULL;
+                    vTaskDelete(NULL);
+                  },
+                  "PUBLISHER-LOOP", 7000, (void*)this, 3, &xPublisherHandle
+                );
+              };
+
+            boolean checkSystemMessage(const char* topic, JsonDocument& doc)
+              {
+                if(!doc.containsKey("type") || !doc["type"].is<const char*>())
+                  return true;  // Skip, if data incorrect
+
+                if(strcmp(_conf->SERVER_TOPIC_LISTEN, topic))
+                  return false;  // Not main server topic
+
+                const char* type = doc["type"];
+                if(!strcmp(type, "HANDSHAKE"))
+                  {
+                    if(!doc.containsKey("id") || !doc["id"].is<const char*>())
+                      return true;
+
+                    const char* confirmationId = doc["id"];
+                    if(strcmp(lastId.c_str(), confirmationId))
+                      return true;
+
+                    isHandshaked = true;  // Handshake done
+                    return true;
+                  }
+                else if(!strcmp(type, "PING"))  // Requested PING -> send PONG
+                  {
+                    char buffer[200];
+
+                    if(doc.containsKey("id") && doc["id"].is<const char*>())
+                      {
+                        char subBuffer[100];
+                        const char idTemplate[] = "{\"id\": \"%s\"}";
+                        const char* id = doc["id"];
+                        sprintf(subBuffer, idTemplate, id);
+                        sprintf(buffer, PONG_MESSAGE_TEMPLATE, subBuffer);
+                      }
+                      else
+                        sprintf(buffer, PONG_MESSAGE_TEMPLATE, "");
+
+                    publish(_conf->SERVER_TOPIC_PUBLISH, buffer);  // Use simple publish instead smartPublish to prevent garbage data after connection fail
+                    return true;
+                  };
+
+                return false;
+              };
+
+            String uuid(void)
+              {
+                uint8_t uuid[16];
+                ESPRandom::uuid(uuid);
+                return ESPRandom::uuidToString(uuid);
+              };
+
+            void callReceiver(void)
+              {
+                if(xInboxHandle != NULL)
+                  return;
+
+                xTaskCreate(
+                  [](void* consumer)
+                  {
+                    MqttConsumer* _instance = static_cast<MqttConsumer*>(consumer);
+                    while(!_instance->inboxBuffer.isEmpty())
+                      {
+                        InboxBufferData data = _instance->inboxBuffer.pop();
+                        deserializeJson(_instance->jsonBuffer, (const byte*)data.payload, data.length);
+                        if(_instance->checkSystemMessage(data.topic, _instance->jsonBuffer))
+                          continue;
+
+                        _instance->userReceiveCallback(data.topic, _instance->jsonBuffer);
+                      };
+
+                    _instance->xInboxHandle = NULL;
+                    vTaskDelete(NULL);
+                  },
+                  "INBOX-LOOP", 5000, (void*)this, 2, &xInboxHandle
+                );
+              };
+
+            void onInboxCallback(char* topic, byte* payload, uint8_t length)
+              {
+                if(inboxBuffer.isFull())
+                  return;
+                inboxBuffer.push(InboxBufferData {topic, payload, length});
+                callReceiver();
               };
 
           public:
             MqttConsumer(void) {};
+
             ~MqttConsumer(void) {};
 
-            void configure(Config& config)
+            void configure(Config& config, receiveCallback_t callback)
               {
-                this->_configuration = &config;
+                this->_conf = &config;
+                this->userReceiveCallback = callback;
                 setClient(wifiClient);
-                setServer(config.host, config.port);
-                setKeepAlive(config.keepAlive);
-                setSocketTimeout(config.socketTimeout);
+                setServer(_conf->HOST, _conf->PORT);
+                setKeepAlive(_conf->KEEPALIVE);
+                setSocketTimeout(_conf->SOCKET_TIMEOUT);
+                setCallback(std::bind(
+                  &MqttConsumer::onInboxCallback,
+                  this,
+                  std::placeholders::_1,
+                  std::placeholders::_2,
+                  std::placeholders::_3
+                ));
               };
 
-            void start(void)
+             boolean smartPublish(const char* topic, const char* payload, boolean retained)
               {
-                xTaskCreate(MqttConsumer::mainLoop, "MqttEngine", 3000, (void*)this, 1, &xEngineHandle);
+                if(publishBuffer.isFull())
+                  return false;
+
+                publishBuffer.push(PublishBufferData {topic, payload, retained});
+                callPublisher();
+                return true;
               };
 
+            void run(void)
+              {
+                xTaskCreate(MqttConsumer::engineLoop, "MQTT-ENGINE", 3000, (void*)this, 1, &xEngineHandle);
+              };
         };
 
-    }
+      class TopicPublisher
+        {
+          private:
+            const char* topic;
+            MqttConsumer* consumer;
+
+          public:
+            TopicPublisher(const char* topic, MqttConsumer& consumer)
+              {
+                this->topic = topic;
+                this->consumer = &consumer;
+              };
+
+            ~TopicPublisher() {};
+
+            void yield(const char* payload, boolean retained=false)
+              {
+                consumer->smartPublish(topic, payload, retained);
+              };
+        };
+    };
 #endif
